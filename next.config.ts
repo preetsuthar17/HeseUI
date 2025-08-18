19c9aa046ec2fc7a4cb86216320005181433d286
preetsuthar17 <preetsutharxd@gmail.com>
2025-08-18 18:08:45 +0530

    Add redirects for GitHub, Twitter, Hextaui, and sponsorship links in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig  = {
+ async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/preetsuthar17/hextaui-x-baseui',
+        permanent: false, 
+      },
+      {
+        source: '/twitter',
+        destination: 'https://x.com/preetsuthar17',
+        permanent: false, 
+      },
+      {
+        source: '/hextaui',
+        destination: 'https://www.hextaui.com',
+        permanent: false, 
+      },
+      {
+        source: "/sponsor",
+        destination: "https://github.com/sponsors/preetsuthar17",
+        permanent: false,
+      }
+    ]
+  },
 };
 
 export default nextConfig;
