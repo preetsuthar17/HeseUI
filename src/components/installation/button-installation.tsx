96a0e28d73607f75269b3a3ae562c101e5ff3744
preetsuthar17 <preetsutharxd@gmail.com>
2025-08-31 13:31:06 +0530

    Add registry instructions to installation components
    - Updated multiple installation components to include instructions for adding a registry to `components.json`.
    - Each component now provides a code block with the necessary JSON configuration for integrating the HeseUI registry.

--- src/components/installation/button-installation.tsx
+++ src/components/installation/button-installation.tsx
@@ -3,6 +3,26 @@ import CodeBlock from '../code-block';
 export function ButtonInstallation() {
   return (
     <>
+      <p>Adding registry to components.json</p>
+      <p>
+        Head over to <kbd>components.json</kbd> and add the following:
+      </p>
+      <CodeBlock
+        code={`{
+  "tailwind": {
+    // code
+  },
+  "aliases": {
+    // code
+  },
+
+  // add the following
+  "registries": {
+    "@heseui": "https://www.heseui.com/r/{name}.json"
+  }
+}`}
+        lang="json"
+      />
       <p>Install component using the CLI</p>
       <CodeBlock
         code={'pnpm dlx shadcn@latest add @heseui/button'}
